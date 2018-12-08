d8197e852ad6ec020ffa7ee2f9c8cce48903411c
julianadenauer <github@julianadenauer.de>
2018-12-08 15:18:11 +0100

    updated bouncing ball example

--- code/04-bouncing-ball.js
+++ code/04-bouncing-ball.js
@@ -3,23 +3,36 @@ var speedX = 5;
 var positionY = 0;
 var speedY = 3;
 
+var bgRed = 8;
+var bgGreen = 100;
+var bgBlue = 230;
+
 function setup() {
 	createCanvas(400, 400);
 }
 
 function draw() {
-	background(8, 100, 230);
+	background(bgRed, bgGreen, bgBlue);
 	ellipse(positionX, positionY, 100, 100);
 
+	// hitting right or left border
 	if (positionX > width || positionX < 0) {
 		speedX = -speedX;
+		changeBackgroundColor();
 	}
 
-  if(positionY > height || positionY < 0) {
+	// hitting top or bottom border
+	if(positionY > height || positionY < 0) {
 		speedY = -speedY;
+		changeBackgroundColor();
 	}
 
 	positionX += speedX;
 	positionY += speedY;
+}
 
+function changeBackgroundColor() {
+	bgRed = random(255);
+	bgGreen = random(255);
+	bgBlue = random(255);
 }
