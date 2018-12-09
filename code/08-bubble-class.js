1388a3e3002b4342af8221613260f8a9fdf41bc3
julianadenauer <github@julianadenauer.de>
2018-12-09 11:46:18 +0100

    updated bubble class

--- code/08-bubble-class.js
+++ code/08-bubble-class.js
@@ -1,3 +1,25 @@
+var bubble1;
+var bubble2;
+var bubble3;
+
+function setup() {
+	createCanvas(400, 400);
+	background(255);
+
+  bubble1 = new Bubble();
+  bubble2 = new Bubble();
+  bubble3 = new Bubble();
+}
+
+function draw() {
+	bubble1.move();
+	bubble1.show();
+	bubble2.move();
+	bubble2.show();
+	bubble3.move();
+	bubble3.show();
+}
+
 class Bubble {
 	constructor() {
 		this.x = 200;
@@ -27,23 +49,4 @@ class Bubble {
 		fill(this.x, this.y, width);
 		ellipse(this.x, this.y, 20, 20);
 	}
-}
-
-
-var bubble1 = new Bubble();
-var bubble2 = new Bubble();
-var bubble3 = new Bubble();
-
-function setup() {
-	createCanvas(400, 400);
-	background(255);
-}
-
-function draw() {
-	bubble1.move();
-	bubble1.show();
-	bubble2.move();
-	bubble2.show();
-	bubble3.move();
-	bubble3.show();
 }
