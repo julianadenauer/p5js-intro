81eae9242badcf4d5615848e8e55e48b5ed2afa3
julianadenauer <github@julianadenauer.de>
2019-01-12 16:00:41 +0100

    added rijksmuseum api example

---
+++ code/17-rijksmuseum-api.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+  httpGet("https://www.rijksmuseum.nl/api/en/collection?q=portrait&key=RVxpJcQK&format=json&ps=99", callback);
+}
+
+function draw() {
+  
+}
+
+function callback(data){
+  print(data); 
+ 
+  // go through all the images
+  for(var i=0; i<10; i++){
+    var imageUrl = data.artObjects[i].webImage.url;
+    print(imageUrl);
+    loadImage(imageUrl, imageCallback);
+  }
+}
+
+function imageCallback(img){
+  // display the image  
+  image(img, 0, 0);
+  
+  // and download it
+  img.save('image', 'png');
+}
