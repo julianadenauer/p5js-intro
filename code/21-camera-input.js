712345c40035c5cccf7ce4e942d822fe12674da4
julianadenauer <github@julianadenauer.de>
2019-01-13 13:54:31 +0100

    added camera input

---
+++ code/21-camera-input.js
@@ -0,0 +1,24 @@
+var capture;
+var pixels;
+
+function setup() {
+  createCanvas(640, 480);
+  
+  capture = createCapture(VIDEO);
+  capture.size(640,480);
+  capture.hide();  
+}
+
+function draw() {
+  background(255);
+  capture.loadPixels();
+  
+  for(var x=0; x < width; x+=20){
+    for(var y=0; y<height; y+=20){
+      	var c = capture.get(x/2,y/2);
+        fill(c);
+      	var diameter = brightness(c)/2;
+  			ellipse(x, y, diameter, diameter);
+    }
+  }
+}
